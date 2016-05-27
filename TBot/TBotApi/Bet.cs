81b8ce82936ff1e906514b8278ba857937ca9dd5
Łukasz Stolarz <lukasz.stolarz@comarch.pl>
2016-05-27 10:14:20 +0200

    Dodanie do repozytorium

---
+++ TBot/TBotApi/Bet.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TBot.TajperApi
+{
+    public class Bet
+    {
+        string player1;
+        string player2;
+
+        string league;
+        DateTime date;
+
+        string type;
+        int bid;
+        int chancesMin;
+        int predictedProfit;
+        double exchange;
+
+        public Bet(Dictionary<string, string> betRawData)
+        {
+            foreach (var item in betRawData)
+            {
+                switch (item.Key)
+                {
+                    case "players":
+                        {
+                            getPlayers(item.Value);
+                        } break;
+                    case "league":
+                        {
+                            this.league = item.Value;
+                        } break;
+                    case "type":
+                        {
+                            this.type = item.Value;
+                        } break;
+
+                    case "date":
+                        {
+                            this.date = Convert.ToDateTime(betRawData["date"]);
+                        } break;
+                    case "bid":
+                        {
+                            this.bid = Convert.ToInt32(getOnlyNumbers(item.Value));
+                        } break;
+                    case "chancesMin":
+                        {
+                            this.chancesMin = Convert.ToInt32(getOnlyNumbers(item.Value));
+                        } break;
+                    case "predictedProfit":
+                        {
+                            this.predictedProfit = Convert.ToInt32(getOnlyNumbers(item.Value));
+                        } break;
+                    case "echange":
+                        {
+                            this.exchange = Convert.ToDouble(getOnlyNumbers(item.Value));
+                        } break;
+                }
+            }
+        }
+
+        string getOnlyNumbers(string str)
+        {
+            return Regex.Match(str, @"\d+").Value;
+        }
+
+        void getPlayers(string players)
+        {
+        }
+    }
+}
---
+++ TBot/TBotApi/DataExtractor.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TBot.TajperApi
+{
+    public class DataExtractor
+    {
+        List<Bet> bets;
+
+        public DataExtractor()
+        {
+            bets = new List<Bet>();
+            getBetDataFromHTML();
+        }
+
+        string GetFilePath()
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    return dlg.FileName;
+                }
+            }
+
+            return null;
+        }
+
+        void getBetDataFromHTML()
+        {
+
+            string betClass = "//div[@class='bet clearfix']"; //todo parametr aplikacji
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.Load(GetFilePath(), Encoding.UTF8);
+
+            List<Bet> betsData = new List<Bet>();
+
+            foreach (HtmlNode betNode in doc.DocumentNode.SelectNodes(betClass))
+            {
+                Dictionary<string, string> betRawData = new Dictionary<string, string>();
+                foreach (HtmlNode itemNode in betNode.ChildNodes)
+                {
+                    if (itemNode.Attributes.Contains("class") && itemNode.Attributes["class"].Value == "title")
+                    {
+                        var playersNode = itemNode.SelectSingleNode(".//strong");
+                        betRawData.Add("players", playersNode.InnerText);
+
+                        foreach (HtmlNode smallNode in itemNode.SelectNodes(".//small"))
+                        {
+                            if (smallNode.Attributes.Contains("class") && smallNode.Attributes["class"].Value == "date")
+                            {
+                                betRawData.Add("date", smallNode.InnerText);
+                            }
+                            else if (smallNode.InnerText.StartsWith("Tenis"))
+                            {
+                                betRawData.Add("league", smallNode.InnerText);
+                            }
+                        }
+                    }
+                    else if (itemNode.InnerText.StartsWith("Typ"))
+                    {
+                        betRawData.Add("type", itemNode.InnerText);
+                    }
+                    else if (itemNode.InnerText.StartsWith("Stawka"))
+                    {
+                        betRawData.Add("bid", itemNode.InnerText);
+                    }
+                    else if (itemNode.InnerText.StartsWith("Ocena"))
+                    {
+                        betRawData.Add("chancesMin", itemNode.InnerText);
+                    }
+                    else if (itemNode.InnerText.StartsWith("Kurs"))
+                    {
+                        betRawData.Add("exchange", itemNode.InnerText);
+                    }
+                    else if (itemNode.InnerText.StartsWith("Przewidywany"))
+                    {
+                        betRawData.Add("predictedProfit", itemNode.InnerText);
+                    }
+                }
+                
+                betsData.Add(new Bet(betRawData));
+            }
+
+            betsData.Clear();
+        }
+    }
+}
